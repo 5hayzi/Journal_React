96763adad33522d92c53cd4e7463f278b77bf890
Sheraz Ahmed <hithisissheraz1947@gmail.com>
2024-08-19 16:28:07 +0500

    Added SideNav.jsx, changes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,28 @@ import './index.css'
 import './App.css'
 
 function App() {
-  const [text, setText] = useState("Enter text here")
 
-  console.log(text);
+  const [mode, setMode] = useState({
+    backgroundColor: 'rgba(0,0,0,1)'
+  }
+  )
   
   return (
-    <> 
-        <div className='p1 bg-black w-2 h-1' id='div'> 
-          <input type="text" name="idk" id="textarea" className='w-80 h-60' placeholder={text}>
-          </input>
-          <button onClick ={()=>{
-            const textarea = document.getElementById("textarea").value
-            const text = textarea.toUpperCase();
-            setText(textarea);
-            const h1 = document.getElementById("h1");
-
-            h1.textContent = `${text}`
-
-          }} className="w-1 h-1 bg-white">click me</button>
-
-          <h1>{text.length} and {text.split(" ").length}</h1>
-          <br/>
-          <h1 id="h1" className='h-auto w-auto text-start text-cyan-50 bg-black'> </h1>
-          </div>   
+    <>
+    <div style= {mode}>
+        <button onClick={()=>{
+          if(mode.backgroundColor === 'rgba(0,0,0,1)'){
+                setMode({backgroundColor: 'rgba(255,255,255,1)'})
+          }
+          else{
+              setMode({backgroundColor: 'rgba(0,0,0,1)'})
+          }
+          
+          
+        }} className='w-50 h-50 bg-green-700'>light Mode</button>
+      </div>
     </>
   )
-  console.log(text);
 }
 
 export default App
