0b2e4abe85024fb7916b1f3f6e68e7c71a79d05c
Sheraz Ahmed <hithisissheraz1947@gmail.com>
2024-09-28 17:01:19 +0500

    -Updated to remove password from persistor

--- FrontEnd/src/Redux/react_component/UserData.js
+++ FrontEnd/src/Redux/react_component/UserData.js
@@ -1,45 +1,44 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const UserData = createSlice({
-  name: 'UserData',
+  name: "UserData",
   initialState: {
     loggedIn: false,
-    name: '',
-    email: '',
-    password:'',
+    name: "",
+    email: "",
+    id: "",
     img: null,
     twoFactor: false,
     rememberMe: false,
-    gender: ''
+    gender: "",
   },
 
   reducers: {
     setValue: (state, action) => {
-        const { name, email, password, img, twoFactor, gender } = action.payload;
+      const { name, email, _id, img, twoFactor, gender } = action.payload;
       state.name = name;
       state.email = email;
-      state.password = password;
+      state.id = _id;
       state.img = img;
       state.twoFactor = twoFactor;
       state.gender = gender;
       state.loggedIn = true;
     },
     setUpdate: (state, action) => {
-      const { name, email, password, img, twoFactor, gender } = action.payload;
-    state.name = name;
-    state.email = email;
-    state.password = password;
-    state.img = img;
-    state.twoFactor = twoFactor;
-    state.gender = gender;
-    state.loggedIn = true;
-  },
-    setRemember: (state, action)=>{
+      const { name, email, img, twoFactor, gender } = action.payload;
+      state.name = name;
+      state.email = email;
+      state.img = img;
+      state.twoFactor = twoFactor;
+      state.gender = gender;
+      state.loggedIn = true;
+    },
+    setRemember: (state, action) => {
       state.rememberMe = action.payload;
-    }
+    },
   },
 });
 
-export const { setValue, setRemember } = UserData.actions;
+export const { setValue, setUpdate, setRemember } = UserData.actions;
 
 export default UserData.reducer;
