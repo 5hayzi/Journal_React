9a294d60964dfb41625b71583c7f6c70660586a9
Sheraz Ahmed <hithisissheraz1947@gmail.com>
2024-09-30 05:03:04 +0500

    -Route to handle delete userAccount controller

--- BackEnd/routes/auth.route.js
+++ BackEnd/routes/auth.route.js
@@ -1,10 +1,16 @@
-import express from 'express';
-import { LogIn, SignUp, upload} from '../controller/auth.controller.js';
+import express from "express";
+import {
+  DeleteUser,
+  LogIn,
+  SignUp,
+  upload,
+} from "../controller/auth.controller.js";
 
 const authRouter = express.Router();
 
 // Apply multer middleware to handle file uploads in signup route
-authRouter.post("/signup", upload.single('img'), SignUp);
+authRouter.post("/signup", upload.single("img"), SignUp);
 authRouter.post("/login", LogIn);
+authRouter.delete("/delete", DeleteUser);
 
 export default authRouter;
