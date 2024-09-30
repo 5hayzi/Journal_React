9f03d7565e8fbad79462f69ebc785c5e8f5befbe
Sheraz Ahmed <hithisissheraz1947@gmail.com>
2024-09-30 17:37:08 +0500

    -adjusted for darkmode, new animated gif added

--- FrontEnd/src/Components/UI/AboutSection.jsx
+++ FrontEnd/src/Components/UI/AboutSection.jsx
@@ -1,3 +1,5 @@
+import gif from "../../assets/gifs/AboutUs.gif";
+
 export default function AboutSection() {
   return (
     <div
@@ -11,8 +13,12 @@ export default function AboutSection() {
         <span className="h-2 text-[6rem] text-black font-semibold text-left absolute -left-[10%] top-[17%] sm:left-6 sm:text-[4rem] sm:top-[17%] z-10 dark:text-[#dbdbdb]">
           Us
         </span>
-        <div className="w-full h-full bg-gray-500 mt-[12%] rounded-lg flex flex-col bg-opacity-10 justify-end hover:z-10 backdrop-blur-sm">
-          <span className="w-2/3 text-lg text-black font-light text-justify p-5 sm:w-full dark:text-[#dbdbdb] sm:text-xs">
+        <div className="w-full h-full bg-gray-500 mt-[12%] rounded-lg flex flex-col  bg-opacity-10 justify-end hover:z-10 dark:hover:bg-opacity-50 backdrop-blur-sm sm:h-[80%] dark:bg-indigo-500">
+          <img
+            src={gif}
+            className="w-[65%] absolute top-0 right-0 sm:top-[20%] sm:w-full"
+          />
+          <span className="w-2/3 text-lg text-black font-light text-justify px-5 pb-5 sm:w-full dark:text-[#dbdbdb] sm:text-xs">
             A web-based journal application for taking notes or saving your
             daily logs. The idea for this app came when we saw people needing to
             download 3<sup>rd</sup> party application on their devices just to
