76a21142eb83bbda52084f9e01555e1fc13ec8b1
Sheraz Ahmed <hithisissheraz1947@gmail.com>
2024-09-30 05:03:17 +0500

    -Route to handle delete all notes controller

--- BackEnd/routes/notes.route.js
+++ BackEnd/routes/notes.route.js
@@ -5,6 +5,7 @@ import {
   deleteNote,
   readNote,
   AllNote,
+  deleteAllNotes,
 } from "../controller/notes.controller.js";
 
 const notesRouter = express.Router();
@@ -12,6 +13,7 @@ const notesRouter = express.Router();
 notesRouter.post("/notes", createNote);
 notesRouter.put("/notes/:id", updateNote);
 notesRouter.delete("/notes/:id", deleteNote);
+notesRouter.delete("/notes", deleteAllNotes);
 notesRouter.get("/notes/:id", readNote);
 notesRouter.get("/notes", AllNote);
 
